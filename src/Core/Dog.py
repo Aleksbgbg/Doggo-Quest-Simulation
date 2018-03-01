57cc4fb21aa05b46a9e0df325f4eb0a2d71841a9
Aleksbgbg <kozichka01@abv.bg>
2018-03-01 11:23:08 +0000

    Add string function and strength property.

--- src/Core/Dog.py
+++ src/Core/Dog.py
@@ -1,8 +1,15 @@
 class Dog:
     def __init__(self, name, strength):
         self._name = name
-        self.strength = strength
+        self._strength = strength
+
+    def __str__(self):
+        return f"Name: {self.name}\nStrength: {self.strength}"
 
     @property
     def name(self):
-        return self.__name
+        return self._name
+
+    @property
+    def strength(self):
+        return self._strength
