89b5580c3c77a59cce57e93319f16254bde5b353
Aleksbgbg <kozichka01@abv.bg>
2018-03-01 13:38:34 +0000

    Change Main to count how many times each player wins.

--- src/Main.py
+++ src/Main.py
@@ -2,5 +2,18 @@ from Core.Game import Game
 
 game = Game()
 
-while game.run():
-    pass
+wins = [0, 0]
+
+for iteration in range(100000):
+    while True:
+        result = game.run()
+
+        if result == -1:
+            continue
+        print(result)
+
+        wins[result] += 1
+        game = Game()
+        break
+
+print(wins)
